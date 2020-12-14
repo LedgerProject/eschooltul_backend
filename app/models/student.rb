b3749da61d9285f156da45300c81fd51f1fded85
Mario Perez <mapreal19@gmail.com>
2020-12-14 21:16:02 +0100

    refactor(rubocop): fixes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,5 @@
 class Student < ApplicationRecord
-  has_many :course_students
+  has_many :course_students, dependent: :delete_all
   has_many :courses, through: :course_students
 
   def full_name
