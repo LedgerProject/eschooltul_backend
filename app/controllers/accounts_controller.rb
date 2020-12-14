cf29c4f37dde16ffb7c22d506a097440f43ade25
exponentia team <exponentiateam@MacBook-Pro-de-exponentia.local>
2020-12-14 20:46:23 +0100

    feat: 🎸 accounts index

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,23 @@
 class AccountsController < AuthenticatedController
   before_action :redirect_unless_director
 
-  def index; end
+  def index
+    @scope = set_scope
+    @search = @scope.ransack(params[:q])
+    @users = @search.result(distinct: true).order(:name).page(params[:page])
+  end
 
   private
 
   def redirect_unless_director
     redirect_to root_path unless current_user.director?
+  end
+
+  def set_scope
+    if params[:role].nil?
+      User.all
+    else
+      User.with_role(params[:role])
+    end
   end
 end
