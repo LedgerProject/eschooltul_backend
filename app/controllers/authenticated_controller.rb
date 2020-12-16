0e08398fb6c1988c663985f7a231fe82ae644842
exponentia team <exponentiateam@MacBook-Pro-de-exponentia.local>
2020-12-16 10:47:51 +0100

    feat: 🎸 students selectable for course

--- app/controllers/authenticated_controller.rb
+++ app/controllers/authenticated_controller.rb
@@ -1,11 +1,20 @@
 class AuthenticatedController < ApplicationController
   before_action :authenticate_user!
   helper_method :current_school
+  helper_method :school_courses
 
   layout "authenticated"
 
   def current_school
     School.first
+  end
+
+  def school_courses
+    if current_user.teacher?
+      current_user.courses
+    else
+      Course.all
+    end
   end
 
   def check_permission
