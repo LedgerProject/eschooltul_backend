48775a8fdff5dbcafbdd9e51a54717619c6ec378
Alejandro <alejandro.adrian@exponentiateam.com>
2021-03-17 11:40:20 +0100

    Files deleted Signed-off-by: Alejandro <alejandro.adrian@exponentiateam.com>

--- app/javascript/components/grades/utils/convertToCSV.js
+++ app/javascript/components/grades/utils/convertToCSV.js
@@ -1,13 +1,13 @@
 import _ from 'lodash/fp';
-
+//TODO - refactor with lodash
 export default function convertToCSV(courseMembers) {
   const dataCSV = [];
 
   courseMembers.forEach((courseMember) => {
     const { student } = courseMember;
-    if (!_.isUndefined(student.terms)) {
-      student.terms.forEach((term) => {
-        if (!_.isUndefined(term.lessons)) {
+    if (courseMember.terms !== undefined) {
+      courseMember.terms.forEach((term) => {
+        if (term.lessons !== undefined) {
           term.lessons.forEach((lesson) => {
             const newLine = {
               Student: `${student.name} ${student.first_surname} ${student.second_surname}`,
@@ -25,3 +25,29 @@ export default function convertToCSV(courseMembers) {
 
   return dataCSV;
 }
+
+/*const setNewLine = (student, term, lesson) => ({
+  Student: `${student.name} ${student.first_surname} ${student.second_surname}`,
+  Term: term.term.name,
+  Lesson: lesson.lesson.name,
+  Mark: lesson.mark.value,
+});
+
+const setLessons = (student, term) => (
+  !_.isUndefined(term.lessons) 
+  ? _.map((lesson) => setNewLine(student, term, lesson))(term.lessons)
+  : null
+);
+
+const setTerms = (courseMember) => (
+  !_.isUndefined(courseMember.terms) 
+  ? _.map((term) => setLessons(courseMember.student, term))(courseMember.terms)
+  : null
+);
+
+const convertToCSV = _.flow(
+  _.map((courseMember) => setTerms(courseMember)),
+  _.compact,
+);
+
+export default convertToCSV;*/
